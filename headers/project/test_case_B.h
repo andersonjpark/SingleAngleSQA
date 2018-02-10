e1571b08aa810222ddb56175c4cf29434f12e6c1
Sherwood Richers <srichers@caltech.edu>
2018-02-09 19:01:51 -0500

    fully working test case

--- headers/project/test_case_B.h
+++ headers/project/test_case_B.h
@@ -1,22 +1,25 @@
 double get_rho(const double r){
-  return -1;//UNCOMMENT exp(lnrho(log(r)));
+  return -1;
 }
 double get_drhodr(const double rrho, const double r){
-  return -1; //UNCOMMENT rrho*lnrho.Derivative(log(r))/r;
+  return -1;
 }
 double get_Ye(const double r){
-  return -1;//UNCOMMENT Ye(r);
+  return -1;
 }
 double get_dYedr(const double r){
-  return -1; //UNCOMMENT Ye.Derivative(r);
+  return -1;
 }
 
+double deltaV(const double E){ // erg
+  return abs(dm21)*cgs::constants::c4 / (2.*E);
+}
 //=======//
 // Ebins //
 //=======//
 void set_Ebins(vector<double>& E){
   const double NEP=8;
-  cout<<"NE="<<NE << " NEP="<<NEP;
+  cout<<endl << "NE="<<NE << " NEP="<<NEP << endl;
   E.resize(NE);
   for(int i=0;i<NE;i++){
     unsigned ind = i;
@@ -24,7 +27,7 @@ void set_Ebins(vector<double>& E){
 
     E[i] = 1.e6*cgs::units::eV * exp(log(2*1000000) + ind*(log(37.48*1000000) - log(2*1000000))/(NE-1))/1000000;
 
-    cout<<E[i]<<endl;
+    cout<<E[i]<<" "<<deltaV(E[i]) << " " << (cgs::constants::hbarc * 2.*M_PI)/deltaV(E[i]) << endl;
     cout.flush();
   }
   cout.flush();
@@ -33,15 +36,12 @@ void set_Ebins(vector<double>& E){
 //===================//
 // Vacuum Potentials //
 //===================//
-double deltaV(const double E){ // erg
-  return abs(dm21)*cgs::constants::c4 / (2.*E);
-}
 
 void set_kV(vector<vector<double> >& kV){
   assert(NF==2);
   for(int i=0;i<NE;i++){
-    kV[i][0] = m1*m1 * cgs::constants::c4 /2./E[0]; //UNCOMMENT E[i];
-    kV[i][1] = kV[i][0] + deltaV(E[0]); //UNCOMMENT E[i];
+    kV[i][0] = m1*m1 * cgs::constants::c4 /2./E[0];
+    kV[i][1] = kV[i][0] + deltaV(E[0]);
   }
 }
 
@@ -50,12 +50,10 @@ void set_kV(vector<vector<double> >& kV){
 //===================//
 double Ve(double rho, double Ye){
   return 1000*deltaV(E[0]);
-  //UNCOMMENT return (M_SQRT2*cgs::constants::GF/cgs::constants::Mp)*rho*Ye;
 }
 
 double dVedr(double rho, double drhodr, double Ye, double dYedr){
   return 0;
-  //UNCOMMENT return (M_SQRT2*cgs::constants::GF/cgs::constants::Mp) * (drhodr*Ye + rho*dYedr );
 }
 
 double Vmu(double rho, double Ye){ return 0.;}
@@ -68,7 +66,8 @@ double dVmudr(double rho, double drhodr, double Ye, double dYedr){ return 0.;}
 //=============================//
 double MU(const double r, const double E){ // erg
   double dV = deltaV(E);
-  return 1e5 * dV * exp(-r*dV/cgs::constants::hbarc / 10.)  / (double)NE;
+  double r_dimless = r*dV / (cgs::constants::hbarc * 2.*M_PI);
+  return 1e4 * dV * exp(-r_dimless / 10.)  / (double)NE;
 }
 double ALPHA(const double r){
   return 4./3.;
@@ -81,9 +80,9 @@ void getP(vector<vector<MATRIX<complex<double>,NF,NF> > >& pmatrixf0, const doub
 	pmatrixf0[matter    ][i][f][fp] = 0;
 	pmatrixf0[antimatter][i][f][fp] = 0;
       }
-    pmatrixf0[matter    ][i][e ][e ]=MU(r, E[0]);           //UNCOMMNT eP[i](r); //
-    pmatrixf0[antimatter][i][e ][e ]=MU(r, E[0]) * ALPHA(r);//UNCOMMENT eBarP[i](r); //
-    pmatrixf0[antimatter][i][mu][mu]=0;//UNCOMMENT xP[i](r);
-    pmatrixf0[matter    ][i][mu][mu]=0;//UNCOMMENT xP[i](r);
+    pmatrixf0[matter    ][i][e ][e ]=MU(r, E[0]);
+    pmatrixf0[antimatter][i][e ][e ]=MU(r, E[0]) * ALPHA(r);
+    pmatrixf0[antimatter][i][mu][mu]=0;
+    pmatrixf0[matter    ][i][mu][mu]=0;
   }
 }
