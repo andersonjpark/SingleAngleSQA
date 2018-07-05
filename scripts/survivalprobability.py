76a70466cde209a321f8ba32cbfc9526794b246b
Sherwood Richers <srichers@caltech.edu>
2018-07-05 16:17:03 -0400

    add axis labels to survival probability plot

--- scripts/survivalprobability.py
+++ scripts/survivalprobability.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 filename = "output/2p.dat"
-r       = np.genfromtxt(filename,usecols=(0))
+r       = np.genfromtxt(filename,usecols=(0))/1e5
 pe      = np.genfromtxt(filename,usecols=(10))
 pa      = np.genfromtxt(filename,usecols=(11))
 peguess = np.genfromtxt(filename,usecols=(13))
@@ -15,5 +15,7 @@ pleguess,= plt.plot(r, peguess, 'b--', label=r"$P_{\nu_e}$ guess")
 plaguess,= plt.plot(r, paguess, 'r--', label=r"$P_{\bar{\nu}_e}$ guess")
 
 plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+plt.xlabel("Distance (km)")
+plt.ylabel("Survival Probability")
 
 plt.savefig("survival_probabilities.pdf",bbox_inches="tight")
