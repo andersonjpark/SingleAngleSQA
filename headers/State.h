2cd4c62d0e451f29f6d98a024932153de543c286
Sherwood Richers <srichers@caltech.edu>
2019-04-02 17:08:37 -0400

    cleaning up State.h

--- headers/State.h
+++ headers/State.h
@@ -14,20 +14,23 @@ class State{
   array<array<MATRIX<complex<double>,NF,NF>,NE>,NM> fmatrixf, fmatrixm;
   hid_t dset_f, dset_r, dset_dr_osc, dset_dr_int, dset_dr_block;
 
-  // temporaries
+  // vacuum matrices set at initial conditions
   array<array<double,NF>,NE> kV;
   array<MATRIX<complex<double>,NF,NF>,NM> UV;
   array<array<MATRIX<complex<double>,NF,NF>,NE>,NM> HfV;
   array<array<MATRIX<complex<double>,NF,NF>,NF>,NE> CV;
   array<array<array<double,NF>,NF>,NE> AV;
-  array<array<MATRIX<complex<double>,NF,NF>,NE>,NM> U0; // mixing angles to MSW basis at initial point
+
+  // mixing angles to MSW basis at initial point
+  array<array<MATRIX<complex<double>,NF,NF>,NE>,NM> U0;
+
+  // potentials and potential derivatives
   array<MATRIX<complex<double>,NF,NF>,NM> VfMSW, dVfMSWdr;
-  array<array< array<double,NF>,NE>,NM> k0;
-  array<array<MATRIX<complex<double>,NF,NF>,NE>,NM> UWBW;
-  array<array<array<MATRIX<complex<double>,NF,NF>,NS>,NE>,NM> Sa;
+  array<array<MATRIX<complex<double>,NF,NF>,NE>,NM> pmatrixf0, pmatrixm0, Scumulative;
+
+  // other matrices
   array<array<double,NM>,NE> dphi_dr_interact, dtheta_dr_interact;
   array<array<double,NM>,NE> dphi_dr_osc,      dtheta_dr_osc;
-  array<array<MATRIX<complex<double>,NF,NF>,NE>,NM> pmatrixf0, pmatrixm0, Scumulative;
 
   State(/*string nulibfilename, string eosfilename, double rho_in, double Ye_in, double T_in, double dr0, double mixing, bool do_interact*/const vector<double>& E){
   /*   r=0; */
@@ -47,30 +50,6 @@ class State{
     HfV = Evaluate_HfV(kV,UV);
     CV  = Evaluate_CV(kV, HfV);
     AV  = Evaluate_AV(kV,HfV,UV);
-    
-  /*   // MSW potential matrix */
-  /*   VfMSW[matter][e][e]=Ve(rho,Ye); */
-  /*   VfMSW[matter][mu][mu]=Vmu(rho,Ye); */
-  /*   VfMSW[antimatter]=-Conjugate(VfMSW[matter]); */
-    
-  /*   // other matrices */
-  /*   for(int m=matter; m<=antimatter; m++){ */
-  /*     for(int i=0;i<NE;i++){ */
-  /* 	MATRIX<complex<double>,NF,NF> Hf0=HfV[m][i]+ VfMSW[m]; */
-  /* 	k0[m][i] = (m==matter? k(Hf0) : kbar(Hf0) ); */
-  /* 	array<double,1> deltak0 = (m==matter ? deltak(Hf0) : deltakbar(Hf0) ); */
-  /* 	array<MATRIX<complex<double>,NF,NF>,NF> C0 = CofactorMatrices(Hf0,k0[m][i]); */
-  /* 	array<array<double,NF>,NF> A0; */
-  /* 	for(int j=0;j<=NF-1;j++){ */
-  /* 	  if(real(C0[j][mu][e]*CV[i][j][mu][e]) < 0.) */
-  /* 	    A0[j][e]=-AV[i][j][e]; */
-  /* 	  else A0[j][e]=AV[i][j][e]; */
-  /* 	  A0[j][mu]=AV[i][j][mu]; */
-  /* 	} */
-  /* 	U0[m][i]=U(deltak0,C0,A0); */
-  /* 	if(m==antimatter) U0[m][i] = Conjugate(U0[m][i]); */
-  /*     } */
-  /*   } */
     
     // set Scumulative to identity
     for(int m=0; m<NM; m++)
@@ -112,7 +91,6 @@ class State{
     dVfMSWdr[matter][mu][e]=0;
     dVfMSWdr[antimatter]=-Conjugate(dVfMSWdr[matter]);
 
-    
     // SI potential
     for(state m=matter; m<=antimatter; m++){
       for(int i=0;i<=NE-1;i++){
