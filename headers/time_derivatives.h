9332c3f88473b6c3ea93d43c1831ffd3cac1587c
Sherwood Richers <srichers@caltech.edu>
2019-04-04 16:38:51 -0400

    moved functions out of main file

---
+++ headers/time_derivatives.h
@@ -0,0 +1,89 @@
+#ifndef TIMEDERIVATIVES_H
+#define TIMEDERIVATIVES_H
+
+//===//
+// K //
+//===//
+array<array<array<array<double,NY>,NS>,NE>,NM> K(double dr, State& s){
+
+  array<array<array<array<double,NY>,NS>,NE>,NM> K;
+
+  
+#pragma omp parallel for collapse(2)
+  for(int m=matter; m<=antimatter; m++){
+    for(int i=0;i<=NE-1;i++){
+      array<double,NF-1> phase;
+      MATRIX<complex<double>,NF,NF> Ha,HB;
+      phase[0] = M_2PI*(s.Y[m][i][msw][4]-s.Y[m][i][msw][5]);
+      Ha[0][1]=0.;
+      for(int j=0;j<=NF-2;j++)
+	for(int k=j+1;k<=NF-1;k++)
+	  for(flavour f=e;f<=mu;f++)
+	    Ha[j][k]+= conj(s.UU[m][i][f][j])*s.dVfMSWdr[m][f][f]*s.UU[m][i][f][k];
+    
+      Ha[0][1] *= I*cgs::constants::hbarc/s.dkk[m][i][0]*exp(I*phase[0]);
+      Ha[1][0] = conj(Ha[0][1]);
+    
+      // HB = -I/cgs::constants::hbarc*Ha*BB;
+      HB[0][0]=-I/cgs::constants::hbarc*( Ha[0][1]*s.BB[m][i][1][0] );
+      HB[0][1]=-I/cgs::constants::hbarc*( Ha[0][1]*s.BB[m][i][1][1] );
+
+      array<double,4> dvdr;
+      dvdr[0]=real(HB[0][1]);
+      dvdr[1]=imag(HB[0][1]);
+      dvdr[2]=real(HB[0][0]);
+      dvdr[3]=imag(HB[0][0]);
+
+      MATRIX<double,3,4> JI = JInverse(s.Y[m][i][msw]);
+      
+      array<double,NF> dkkdr = dkdr(s.UU[m][i],s.dVfMSWdr[m]);
+      array<MATRIX<complex<double>,NF,NF>,NF> dCCdr = CofactorMatricesDerivatives(s.Hf[m][i],s.dVfMSWdr[m],dkkdr);
+      array<double,NF> QQ =  Q(s.UU[m][i],s.dkk[m][i],s.CC[m][i],dCCdr);
+
+      for(int j=0;j<=2;j++){
+	K[m][i][msw][j]=0.;
+	for(int k=j;k<=3;k++)
+	  K[m][i][msw][j] += JI[j][k]*dvdr[k];
+      }
+      K[m][i][msw][3] = 0.;
+      K[m][i][msw][4] = (s.kk[m][i][0]+QQ[0])/M_2PI/cgs::constants::hbarc;
+      K[m][i][msw][5] = (s.kk[m][i][1]+QQ[1])/M_2PI/cgs::constants::hbarc;
+      for(int j=0;j<NY;j++)
+	K[m][i][msw][j]*=dr;
+
+      
+      // *********************
+      // SI part of solution *
+      // *********************
+      Ha = Adjoint(s.UWBW[m][i])*s.VfSI[m]*s.UWBW[m][i];
+
+      K[m][i][si][4]=dr*real(Ha[0][0])/(M_2PI*cgs::constants::hbarc);
+      K[m][i][si][5]=dr*real(Ha[1][1])/(M_2PI*cgs::constants::hbarc);
+    
+      HB[0][0]=-I/cgs::constants::hbarc*( Ha[0][1]*s.Sa[m][i][si][1][0] );
+      HB[0][1]=-I/cgs::constants::hbarc*( Ha[0][1]*s.Sa[m][i][si][1][1] );
+    
+      //array<double,4> dvdr;
+      dvdr[0]=real(HB[0][1]);
+      dvdr[1]=imag(HB[0][1]);
+      dvdr[2]=real(HB[0][0]);
+      dvdr[3]=imag(HB[0][0]);
+    
+      //MATRIX<double,3,4>
+      JI = JInverse(s.Y[m][i][si]);
+    
+      for(int j=0;j<=2;j++){
+	K[m][i][si][j]=0.;
+	for(int k=j;k<=3;k++) K[m][i][si][j]+=JI[j][k]*dvdr[k];
+	K[m][i][si][j]*=dr;
+      }
+    
+      K[m][i][si][3]=0.;
+    }
+  }
+  
+  return K;
+}// end of K function
+
+
+#endif
--- sqa2.psma.cpp
+++ sqa2.psma.cpp
@@ -58,68 +58,10 @@ using std::array;
 #include "headers/MNR.h"
 #include "headers/misc.h"
 #include "headers/State.h"
-
-
-array<array<array<array<double,NY>,NS>,NE>,NM> K(double dr, State& s);
-
+#include "headers/time_derivatives.h"
 #include "headers/output.h"
 #include "headers/project/albino.h"
-
-void interact(double dr, State& s,
-	      const array<DISCONTINUOUS,NE>& eD,
-	      const array<DISCONTINUOUS,NE>& eBarD,
-	      const array<DISCONTINUOUS,NE>& xD){
-  
-  // save old fmatrix
-  array<array<MATRIX<complex<double>,NF,NF>,NE>,NM> old_fmatrixf = s.fmatrixf;
-
-  // let neutrinos interact
-  my_interact(s.fmatrixf, dr, s, eD,eBarD,xD);
-  
-  // loop through getting rotation matrices
-  double hold[4];
-  double hnew[4];
-  for(int i=0; i<NE; i++){
-    for(state m=matter; m<=antimatter; m++){
-      pauli_decompose(old_fmatrixf[m][i], hold);
-      pauli_decompose(  s.fmatrixf[m][i], hnew);
-
-      // get the theta and phi contribution
-      double oldmag2   = hold[0]*hold[0] + hold[1]*hold[1] + hold[2]*hold[2];
-      double newmag2   = hnew[0]*hnew[0] + hnew[1]*hnew[1] + hnew[2]*hnew[2];
-      if(oldmag2==0 or newmag2==0){
-	continue;
-      }
-      s.dtheta_dr_interact[i][m] = (acos(hnew[2]/sqrt(newmag2)) - acos(hold[2]/sqrt(oldmag2))) / dr;
-      s.dphi_dr_interact[i][m] = (atan2(hnew[1],hnew[0]) - atan2(hold[1],hold[0])) / dr;
-
-      // get the axis of rotation
-      double lrot[3];
-      lrot[0] =   hold[1]*hnew[2] - hold[2]*hnew[1] ;
-      lrot[1] = -(hold[0]*hnew[2] - hold[2]*hnew[0]);
-      lrot[2] =   hold[0]*hnew[1] - hold[1]*hnew[0] ;
-      double lmag2 = lrot[0]*lrot[0] + lrot[1]*lrot[1] + lrot[2]*lrot[2];
-      double lmag = sqrt(lmag2);
-      double sinalpha = sqrt(lmag2 / (oldmag2*newmag2));
-      if(lmag > 0)
-	for(unsigned i=0; i<3; i++) lrot[i] /= sqrt(lmag);
-      else continue;
-
-      // get the rotation operator in the flavor basis
-      double alpha = asin(sinalpha);
-      complex<double> Rcoeff[4];
-      for(int i=0; i<3; i++) Rcoeff[i] = -I * sin(alpha/2.) * lrot[i];
-      Rcoeff[3] = cos(alpha/2.);
-      
-      MATRIX<complex<double>,NF,NF> R;
-      pauli_reconstruct(Rcoeff, R);
-
-      // apply to Scumulative
-      s.Scumulative[m][i] = MATRIX<complex<double>,NF,NF>(Adjoint(s.U0[m][i]) * R * s.U0[m][i] * s.Scumulative[m][i]);
-    }
-  }
-}
-
+#include "headers/interact.h"
 #include "headers/nulib_interface.h"
 
 //======//
@@ -468,88 +410,4 @@ int main(int argc, char *argv[]){
   return 0;
 }
 
-
-//===//
-// K //
-//===//
-array<array<array<array<double,NY>,NS>,NE>,NM> K(double dr, State& s){
-
-  array<array<array<array<double,NY>,NS>,NE>,NM> K;
-
-  
-#pragma omp parallel for collapse(2)
-  for(int m=matter; m<=antimatter; m++){
-    for(int i=0;i<=NE-1;i++){
-      array<double,NF-1> phase;
-      MATRIX<complex<double>,NF,NF> Ha,HB;
-      phase[0] = M_2PI*(s.Y[m][i][msw][4]-s.Y[m][i][msw][5]);
-      Ha[0][1]=0.;
-      for(int j=0;j<=NF-2;j++)
-	for(int k=j+1;k<=NF-1;k++)
-	  for(flavour f=e;f<=mu;f++)
-	    Ha[j][k]+= conj(s.UU[m][i][f][j])*s.dVfMSWdr[m][f][f]*s.UU[m][i][f][k];
-    
-      Ha[0][1] *= I*cgs::constants::hbarc/s.dkk[m][i][0]*exp(I*phase[0]);
-      Ha[1][0] = conj(Ha[0][1]);
-    
-      // HB = -I/cgs::constants::hbarc*Ha*BB;
-      HB[0][0]=-I/cgs::constants::hbarc*( Ha[0][1]*s.BB[m][i][1][0] );
-      HB[0][1]=-I/cgs::constants::hbarc*( Ha[0][1]*s.BB[m][i][1][1] );
-
-      array<double,4> dvdr;
-      dvdr[0]=real(HB[0][1]);
-      dvdr[1]=imag(HB[0][1]);
-      dvdr[2]=real(HB[0][0]);
-      dvdr[3]=imag(HB[0][0]);
-
-      MATRIX<double,3,4> JI = JInverse(s.Y[m][i][msw]);
-      
-      array<double,NF> dkkdr = dkdr(s.UU[m][i],s.dVfMSWdr[m]);
-      array<MATRIX<complex<double>,NF,NF>,NF> dCCdr = CofactorMatricesDerivatives(s.Hf[m][i],s.dVfMSWdr[m],dkkdr);
-      array<double,NF> QQ =  Q(s.UU[m][i],s.dkk[m][i],s.CC[m][i],dCCdr);
-
-      for(int j=0;j<=2;j++){
-	K[m][i][msw][j]=0.;
-	for(int k=j;k<=3;k++)
-	  K[m][i][msw][j] += JI[j][k]*dvdr[k];
-      }
-      K[m][i][msw][3] = 0.;
-      K[m][i][msw][4] = (s.kk[m][i][0]+QQ[0])/M_2PI/cgs::constants::hbarc;
-      K[m][i][msw][5] = (s.kk[m][i][1]+QQ[1])/M_2PI/cgs::constants::hbarc;
-      for(int j=0;j<NY;j++)
-	K[m][i][msw][j]*=dr;
-
-      
-      // *********************
-      // SI part of solution *
-      // *********************
-      Ha = Adjoint(s.UWBW[m][i])*s.VfSI[m]*s.UWBW[m][i];
-
-      K[m][i][si][4]=dr*real(Ha[0][0])/(M_2PI*cgs::constants::hbarc);
-      K[m][i][si][5]=dr*real(Ha[1][1])/(M_2PI*cgs::constants::hbarc);
-    
-      HB[0][0]=-I/cgs::constants::hbarc*( Ha[0][1]*s.Sa[m][i][si][1][0] );
-      HB[0][1]=-I/cgs::constants::hbarc*( Ha[0][1]*s.Sa[m][i][si][1][1] );
-    
-      //array<double,4> dvdr;
-      dvdr[0]=real(HB[0][1]);
-      dvdr[1]=imag(HB[0][1]);
-      dvdr[2]=real(HB[0][0]);
-      dvdr[3]=imag(HB[0][0]);
-    
-      //MATRIX<double,3,4>
-      JI = JInverse(s.Y[m][i][si]);
-    
-      for(int j=0;j<=2;j++){
-	K[m][i][si][j]=0.;
-	for(int k=j;k<=3;k++) K[m][i][si][j]+=JI[j][k]*dvdr[k];
-	K[m][i][si][j]*=dr;
-      }
-    
-      K[m][i][si][3]=0.;
-    }
-  }
-  
-  return K;
-}// end of K function
 
