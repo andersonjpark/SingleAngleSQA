4021c3a3db9becb6862e3e4f701881a85c385e72
Andy Park <andersonjpark@berkeley.edu>
2020-06-03 10:14:24 +0900

    variable name change

--- src/nulib_interface.h
+++ src/nulib_interface.h
@@ -176,7 +176,7 @@ class EAS{
 public:
 
 	vector<double> eos_variables;
-	double munue_kT, eta, Edensity;
+	double munue_kT, eta, E_density_electron;
 
 	array<array<double,NE>,NS_NULIB> absorption_opacity, emissivities, scattering_opacity, delta;
 
