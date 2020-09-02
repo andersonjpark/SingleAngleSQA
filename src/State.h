1515a0f6f1898f12fb3138b22f5e18e6f0842ae4
Andy Park <andersonjpark@berkeley.edu>
2020-09-02 14:21:13 -0700

    removed *100 to now run the simulation realtime

--- src/State.h
+++ src/State.h
@@ -137,7 +137,7 @@ public:
 		for(int i=0; i<=NE-1; i++) {
 			double two_loop_contribution_e = 0;
 			if (do_two_loop_contribution==true){
-				two_loop_contribution_e = 8.0*M_SQRT2*cgs::constants::GF*Ecom[i]*eas.E_density_electron/3.0/cgs::constants::Mw/cgs::constants::Mw*100;
+				two_loop_contribution_e = 8.0*M_SQRT2*cgs::constants::GF*Ecom[i]*eas.E_density_electron/3.0/cgs::constants::Mw/cgs::constants::Mw;
 			}
 			VfEde[i ][e ][e ] = two_loop_contribution_e;
 			VfEde[i ][mu][mu] = 0;
@@ -187,7 +187,7 @@ public:
 				MATRIX<complex<double>,NF,NF> VfSIE = (MBackground[m][i0][0] - MBackground[m][i0][1]) * sqrt(2.)*cgs::constants::GF;
 				MATRIX<complex<double>,NF,NF> VfEdnu = MATRIX<complex<double>,NF,NF>();
 				if (do_two_loop_contribution==true) {
-				  VfEdnu = (MBackground[m][i0][0]*Ecom[i0])*8.0*M_SQRT2*cgs::constants::GF/3.0/cgs::constants::Mw/cgs::constants::Mw*100;
+				  VfEdnu = (MBackground[m][i0][0]*Ecom[i0])*8.0*M_SQRT2*cgs::constants::GF/3.0/cgs::constants::Mw/cgs::constants::Mw*Ecom[i0];
 				}
 				VfSI[matter] += (m==matter ? VfSIE : -Conjugate(VfSIE));
 				VfSI[matter] += (m==matter ? VfEdnu : -Conjugate(VfEdnu));
@@ -195,7 +195,7 @@ public:
 		}
 		// convert comoving-frame potential to lab-frame potential
 		VfSI[matter] *= Ecom_Elab;
-
+		
 		// set antimatter potential
 		VfSI[antimatter]=-Conjugate(VfSI[matter]);
 	}
